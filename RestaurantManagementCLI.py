aed0983053aa03ba1966cfa07aa09b59f106b2a7
Swaraj Hota <swarajhota353@gmail.com>
2020-10-07 23:31:59 +0530

    add initial menu

--- RestaurantManagementCLI.py
+++ RestaurantManagementCLI.py
@@ -1,51 +1,28 @@
 from DataIO import *
 from Orders import *
 
+def admin():
+    pass
+
+def user():
+    pass
+
 def main():
-    ingredientsList = loadIngredients()
-    foodMenu        = loadFoodMenu()
-    prices          = loadPrices()
-    prevData        = loadPrevData()    # ingredients sold in the past 3 days
-
-    ### Add Admin Login here to change prices
-
-    ###
-
-    print("Enter -1 to stop, foodId and foodAmount to continue, enter 0 and 0 to end day and save data")
-
-    foodId        = 0
-    totalConsumed = 0
-
-    while(True):
-        foodId = int(input())
-        if(foodId == -1):
-            break
-        foodAmount = int(input())
-
-        if(foodId == 0):    # Write into daily consumption data
-            writeDailyData(totalConsumed,prevData)    # adds a row containing the consumption data
-            totalConsumed = 0
-            prevData = loadPrevData()
-
-        availIngredients = 1
-        consumedIngredients = fetchRequiredIngredients(foodId,foodMenu) * foodAmount
-        ingredientsList = ingredientsList - consumedIngredients
-        totalConsumed = totalConsumed + consumedIngredients
-
-        ### Insufficient Ingredients
-        for x in ingredientsList:
-            if(x<0):
-                print("Insufficient ingredients\n")
-                availIngredients = 0
-                ingredientsList = ingredientsList + consumedIngredients
-        ###
-
-        if(availIngredients == 1 and foodId != 0):
-            updateIngredients(ingredientsList)
-            generateBill(foodId,foodAmount,prices)
-            orderIngredients = isBelowThreshold(ingredientsList)
-            sendOrder(orderIngredients,prevData,prices)
-
-
+    while True:
+        choice = 0
+        print("\nRestaurant Management System v0.1")
+        print("=================================================")
+        print("1. Admin")
+        print("2. User")
+        print("Enter Choice (0 to exit): ")
+        if choice == 1:
+            admin()
+        elif choice == 2:
+            user()
+        elif choice == 0:
+            exit()
+        else
+            print("Invalid Choice!")
+ 
 if __name__ == "__main__":
     main()
