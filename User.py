4d6d3f0fbee13e869b2cb0169e5f8628702ad8aa
Zecadron <adiktripathy@gmail.com>
2020-10-08 12:51:17 +0530

    Misc fixes

--- User.py
+++ User.py
@@ -8,13 +8,26 @@ Bill = Bill()
 
 def checkAvailability(dishId, dishQuantity):
     consumedIngredientsId = DishIngredients.getIngredientIds(dishId)
-    for x in consumedIngredientsId:
+    for ingId in consumedIngredientsId:
         if DishIngredients.getQuantity(dishId,x)*dishQuantity > Ingredients.getStock():
             return False
     return True
 
-def generateBill(inputList):
+def deductIngredients(dishId, dishQuantity):
+    consumedIngredientsId = DishIngredients.getIngredientIds(dishId)
+    for ingId in consumedIngredientsId:
+        consumedStock = DishIngredients.getQuantity(dishId, ingId) * dishQuantity
+        newStock = Ingredients.getStock(ingId) - consumedStock
+        Ingredients.setStock(ingId,newStock)
     pass
+
+def generateBill(inputList):
+    Total = 0
+    for dishId in inputList:
+        Total = Total + inputList[dishId]
+
+    billText = "Sample Text"
+    return billText
 
 def endDay():
     print("The End\n")
